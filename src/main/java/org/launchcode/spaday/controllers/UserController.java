5fc4fbb43b9c11d95e5d33e1e1745ddd08f42a2d
rockit-man <demetrius.esq@gmail.com>
2021-06-13 11:44:31 -0500

    validation functions as needed

--- src/main/java/org/launchcode/spaday/controllers/UserController.java
+++ src/main/java/org/launchcode/spaday/controllers/UserController.java
@@ -3,10 +3,13 @@ package org.launchcode.spaday.controllers;
 import org.launchcode.spaday.models.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("user")
@@ -14,23 +17,17 @@ public class UserController {
 
     @GetMapping("/add")
     public String displayAddUserForm(Model model) {
-//        User user = new User();
-//        model.addAttribute("user", user);
-        return "/user/add";
+        model.addAttribute(new User());
+        return "user/add";
     }
 
     @PostMapping
-    public String processAddUserForm(Model model, @ModelAttribute User user, String verify) {
-        model.addAttribute("user", user);
-        model.addAttribute("verify", verify);
-//        model.addAttribute("username", user.getUsername());
-        model.addAttribute("email", user.getEmail());
-
-        if (user.getPassword().equals(verify)) {
-            return "user/index";
-        } else {
-            model.addAttribute("error", "Passwords do not match!");
+    public String processAddUserForm(Model model, @ModelAttribute @Valid User user, Errors errors) {
+        if (errors.hasErrors()) {
             return "user/add";
+        }
+        else {
+            return "user/index";
         }
     }
 }
--- src/main/java/org/launchcode/spaday/models/User.java
+++ src/main/java/org/launchcode/spaday/models/User.java
@@ -1,17 +1,36 @@
 package org.launchcode.spaday.models;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 public class User {
 
+    @NotEmpty(message = "Please enter a username.")
+    @Size(min = 5, max = 15)
     private String username;
+    @Email(message = "Please provide a valid email address.")
     private String email;
+    @Size(min = 6, message = "Must have a length greater than 6.")
+    @NotEmpty(message = "Please enter a password.")
     private String password;
+    @NotNull(message = "Passwords do not match.")
+    private String verifyPassword;
 
     public User() {}
 
     public User(String username, String email, String password) {
+        this();
         this.username = username;
         this.email = email;
         this.password = password;
+    }
+
+    private void checkPassword() {
+        if (this.password != null && !this.password.equals(verifyPassword)) {
+            verifyPassword = null;
+        }
     }
 
     public String getUsername() {
@@ -36,5 +55,15 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+        checkPassword();
+    }
+
+    public String getVerifyPassword() {
+        return verifyPassword;
+    }
+
+    public void setVerifyPassword(String verifyPassword) {
+        this.verifyPassword = verifyPassword;
+        checkPassword();
     }
 }
